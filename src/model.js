fd34ddf8531032408a42c1c7cbf20559d9ecc544
rehmanshams <rehman.alphasquad@gmail.com>
2022-11-16 18:59:08 +0500

    message ifx

--- src/model.js
+++ src/model.js
@@ -16,8 +16,13 @@ const model = () => {
               <div className="relative px-[32px] py-[24px]">
                 <div className="flex flex-col max-w-[350px] items-center justify-center w-full">
                   <h1 className="pt-2 font-thin text-black text-[18px] ">
-                  Congratulation You Win The Game 
+                    Congratulation You Win The Game
                   </h1>
+                  <a href="new-puzzle-app.vercel.app/">
+                    <button className="border-black w-24 mt-2 rounded-md bg-transparent cursor border-[1px]">
+                      back
+                    </button>
+                  </a>
                 </div>
               </div>
             </div>
